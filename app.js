900c2de2a4782b7480f31f940d1c6da04050f258
Isaiah Tadiwanashe Chiraira <itchiraira@gmail.com>
2022-05-24 13:33:32 +0200

    completed sign up feature

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 const express = require('express')
+const mysql = require("mysql")
 const fs = require('fs')
 const app = express()
 const path = require('path');
 const port = 5000
 
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+app.use(express.static(__dirname + '/models'));
+const User = require('./models/user')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const JWT_SECRET = 'sdjkfh8923yhjdksbfma@#*(&@*!^#&@bhjb2qiuhesdbhjdsfg839ujkdhfjk'
+
+mongoose.connect("mongodb+srv://Tadiwa:test123@cluster0.vlabe.mongodb.net/Cluster0?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+
+})
+
+  .then(() => {
+    console.log("connected to db")
+  })
+  .catch((err) => {
+    console.log(err)
+  });
+
+app.use('/', express.static(path.join(__dirname, 'static')))
+app.use(bodyParser.json())
 
 
 
@@ -18,12 +43,7 @@ app.get('/screen', (req, res) => {
   res.sendFile(index);
 });
 
-//Login
-app.use(express.static(__dirname + '/public'));
-app.get('/login', (req, res) => {
-  const index = path.join(__dirname, 'views', 'login.html');
-  res.sendFile(index);
-});
+
 
 //SignUp
 app.use(express.static(__dirname + '/public'));
@@ -32,6 +52,43 @@ app.get('/signup', (req, res) => {
   res.sendFile(index);
 });
 
+app.use(express.static(__dirname + '/public'));
+app.post('/signup', async (req, res) => {
+  const { username, password: plainTextPassword } = req.body
+
+  if (!username || typeof username !== 'string') {
+    return res.json({ status: 'error', error: 'Invalid username' })
+  }
+
+  if (!plainTextPassword || typeof plainTextPassword !== 'string') {
+    return res.json({ status: 'error', error: 'Invalid password' })
+  }
+
+  if (plainTextPassword.length < 5) {
+    return res.json({
+      status: 'error',
+      error: 'Password too small. Should be atleast 6 characters'
+    })
+  }
+
+  const password = await bcrypt.hash(plainTextPassword, 10)
+
+  try {
+    const response = await User.create({
+      username,
+      password
+    })
+    console.log('User created successfully: ', response)
+  } catch (error) {
+    if (error.code === 11000) {
+      // duplicate key
+      return res.json({ status: 'error', error: 'Username already in use' })
+    }
+    throw error
+  }
+
+  res.json({ status: 'ok' })
+})
 
 
 app.listen(port, () => {
--- public/javascripts/login.js
+++
@@ -1,5 +0,0 @@
-function move() {
-  window.location.href = '/signup'
-}
-
-document.getElementById("clickMe").onclick = move
