c97a08c2f1f124439ebbd1fba048849971de7bef
tirie26 <88463175+tirie26@users.noreply.github.com>
2022-05-26 23:15:45 +0200

    Revert "Revert "Hardware keyboard feature""

--- src/public/javascript/screen.js
+++ src/public/javascript/screen.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", ()=> {
+    const numberOfGridEntries = 30;
     createSquares();
     function createSquares() {
         const gameBoard= document.getElementById("checkerboard");
 
-        for (let i=0; i<30; i++){
+        for (let i=0; i < numberOfGridEntries; i++) {
             let square = document.createElement("div");
             square.classList.add("square");
             square.setAttribute("id", i+1);
