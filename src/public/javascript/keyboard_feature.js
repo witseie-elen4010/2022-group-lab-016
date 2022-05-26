d45a19d44b5fe6a92652748ea7f7bec5e76da4af
BrianConf <brain.confidence@gmail.com>
2022-05-26 21:05:57 +0200

    added the screen response functionality

--- src/public/javascript/keyboard_feature.js
+++ src/public/javascript/keyboard_feature.js
@@ -1,6 +1,14 @@
 'use strict'
 
+let SCREEN_CURSOR = 0
+const BUTTON_LENGTH = 5
+
 const game_buttons = Array.from(document.getElementsByClassName('button'));
+
+const get_element_function = function (value) {
+  const element = String(value);
+  return document.getElementById(element);
+}
 
 game_buttons.map(button => {
   button.addEventListener('click', (e) => {
@@ -13,9 +21,17 @@ game_buttons.map(button => {
         break
       case 'DEL':
         // allow the user to delete some elements
+        get_element_function(SCREEN_CURSOR).innerText = '\xa0';
+         if (SCREEN_CURSOR > 0) {
+             SCREEN_CURSOR--;
+         } 
         break
       default:
         // enter characters on the screen 
+        if (SCREEN_CURSOR <  30) {
+          SCREEN_CURSOR++;
+          get_element_function(SCREEN_CURSOR).innerText = element;
+       }
         break
     }
   })
