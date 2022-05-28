668192b9ee3b27e9b6c35fb68e05829431e0c58e
Isaiah Tadiwanashe Chiraira <itchiraira@gmail.com>
2022-05-28 06:16:11 +0200

    fixed routes

--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -1,12 +1,14 @@
-'use strict'
+// 'use strict'
 
-const path = require('path')
-const express = require('express')
-const app = express()
-const mainRouter = express.Router()
-mainRouter.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'views', 'signin.html'))
-})
-app.use(mainRouter)
+// const path = require('path')
+// const express = require('express')
+// const app = express()
+// const mainRouter = express.Router()
+// app.use(express.static(__dirname + '/public'));
+// mainRouter.get('/', function (req, res) {
+//   const index = path.join(__dirname, 'routes', 'views', 'signin');
+//   res.sendFile(index);
+// })
+// app.use(mainRouter)
 
-module.exports = mainRouter
+// module.exports = mainRouter
