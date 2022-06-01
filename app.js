cb5825bbc91e0c8e3cb865b65eeac1ae154d213a
Karabo <kkgatla09@gmail.com>
2022-06-01 18:30:40 +0200

    Add reset password functionality

--- app.js
+++ app.js
@@ -143,3 +143,40 @@ app.post('/signup', async (req, res) => {
 app.listen(port, () => {
   console.log('Express server running on port', port)
 })
+
+//ResetPassword
+app.use(express.static(__dirname + '/public'));
+app.post('/reset_password', async (req, res) => {
+  const { token, newpassword: plainTextPassword } = req.body
+
+  if (!plainTextPassword || typeof plainTextPassword !== 'string') {
+    return res.json({ status: 'error', error: 'Invalid password' })
+  }
+
+  if (plainTextPassword.length < 5) {
+    return res.json({
+      status: 'error',
+      error: 'Password too small. Should be atleast 6 characters'
+    })
+  }
+
+  try {
+    const user = jwt.verify(token, JWT_SECRET)
+
+    const _id = user.id
+
+    const password = await bcrypt.hash(plainTextPassword, 10)
+
+    await User.updateOne(
+      { _id },
+      {
+        $set: { password }
+      }
+    )
+
+    res.json({ status: 'ok' })
+  } catch (error) {
+    console.log(error)
+    res.json({ status: 'error', error: ';))' })
+  }
+})
