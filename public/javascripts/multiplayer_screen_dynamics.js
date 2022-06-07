d2852c928de1f610cd50faac129ed67ce2a052c4
BrianConf <brain.confidence@gmail.com>
2022-06-07 23:27:45 +0200

    Added multiplayer

--- public/javascripts/multiplayer_screen_dynamics.js
+++ public/javascripts/multiplayer_screen_dynamics.js
@@ -6,13 +6,13 @@ let updateOpponentScreen = function(opponentGuess){
     let n = 0;
     for( let i = OPPONENT_SCREEN_START; i <= OPPONENT_SCREEN_END; i++){
       if(opponentGuess[n] === '1'){
-        get_element_function(i).style.animation =  'mymove 1s 1 linear forwards ' + String(1.5*(n + 1)) + 's'
+        get_element_function('board' + (i - 1)).style.animation =  'mymove 1s 1 linear forwards ' + String(1.5*(n + 1)) + 's'
       }
       else if (opponentGuess[n] === '2'){
-        get_element_function(i).style.animation =  'mymove2 1s 1 linear forwards ' + String(1.5*(n + 1)) + 's'
+        get_element_function('board' + (i - 1)).style.animation =  'mymove2 1s 1 linear forwards ' + String(1.5*(n + 1)) + 's'
       }
       else{
-        get_element_function(i).style.animation =  'mymove3 1s 1 linear forwards ' + String(1.5*(n + 1)) + 's'
+        get_element_function('board' + (i - 1)).style.animation =  'mymove3 1s 1 linear forwards ' + String(1.5*(n + 1)) + 's'
       }
       if((i % BUTTON_LENGTH) === 0) break;
       n++;
