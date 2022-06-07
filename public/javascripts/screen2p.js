00b4d9e8b042c3cc11a584910a316f3f1bfef729
Njabuloo <Masilelanjabulo01@gmail.com>
2022-06-07 18:32:35 +0200

    Multiplayer screen updated

--- public/javascripts/screen2p.js
+++ public/javascripts/screen2p.js
@@ -11,16 +11,3 @@ document.addEventListener("DOMContentLoaded", ()=> {
         }
     }
 })
-document.addEventListener("DOMContentLoaded", ()=> {
-    createSquares();
-    function createSquares() {
-        const gameBoard= document.getElementById("checkerboard_2");
-
-        for (let i=30; i<60; i++){
-            let square = document.createElement("div");
-            square.classList.add("square");
-            square.setAttribute("id", i+1);
-            gameBoard.appendChild(square);
-        }
-    }
-})
